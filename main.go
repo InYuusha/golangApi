ed51446f4f8ba7d65c2554e89d17adba9a56b2f9
InYuusha <asuse975@gmail.com>
2021-05-14 03:10:15 +0530

    put operation

--- main.go
+++ main.go
@@ -55,6 +55,19 @@ func createBook(w http.ResponseWriter, r* http.Request){
 //update book
 func updateBook(w http.ResponseWriter, r* http.Request){
 	w.Header().Set("Content-Type","application/json")
+	params:=mux.Vars(r)
+	for index,item := range books {
+		if item.ID==params["id"]{
+			books = append(books[:index],books[index+1:]...)
+			var book Book
+			_=  json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(1000000))
+			books =append(books,book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 
 }
 //delete book
@@ -65,6 +78,7 @@ func deleteBook(w http.ResponseWriter, r* http.Request){
 	for index,item := range books {
 		if item.ID==params["id"]{
 			books = append(books[:index],books[index+1:]...)
+			break
 		}
 	}
 	json.NewEncoder(w).Encode(books)
